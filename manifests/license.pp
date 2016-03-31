1580972835a149b1f44c47183c3b7e59345b75b1
Bryan Belanger <bbelanger@azcender.com>
2016-03-30 22:28:56 -0400

    Test without wait

--- manifests/license.pp
+++ manifests/license.pp
@@ -3,11 +3,9 @@ class artifactory::license {
   # Only run if pro
   if $::artifactory::is_pro {
     exec { 'wait for service':
-      path      => '/bin',
-      #command   => "curl -uadmin:password -X POST -H 'Content-Type: application/json' -d '{\"licenseKey\": \"${::artifactory::license_key}\"}' http://localhost:8081/api/system/license",
-      onlyif    => 'test `curl -sL -w "%{http_code}\\n" http://localhost:8081/api/system/license -o /dev/null` != \'200\'',
-      tries     => 12,
-      try_sleep => 10,
+      path    => '/bin',
+      command => "curl -uadmin:password -X POST -H 'Content-Type: application/json' -d '{\"licenseKey\": \"${::artifactory::license_key}\"}' http://localhost:8081/api/system/license",
+      onlyif  => 'test `curl -uadmin:password -sL -w "%{http_code}\\n" http://localhost:8081/artifactory/api/system/license -o /dev/null` == \'200\'',
     }
   }
 }
