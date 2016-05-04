7b66de4350d5ca6203a956ce10e1ab7687577540
Ron Compos <rcompos@gmail.com>
2016-05-04 14:00:10 -0400

    Create plugins dir.

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,9 +3,18 @@
 # This class is called from artifactory for service config.
 #
 class artifactory::config {
+
+  # Create the plugins directory
+  file { $::artifactory::plugins_dir:
+    ensure => directory,
+    owner  => artifactory,
+    group  => artifactory,
+    require => Package[$::artifactory::package_name],
+  }
+
   file { "${::artifactory::plugins_dir}/zip_upload.groovy":
     source  => 'puppet:///modules/artifactory/zip_upload.groovy',
-    require => Package[$::artifactory::package_name],
+    require => File[$::artifactory::plugins_dir],
     #notify  => Service['artifactory'],
   }
 }
