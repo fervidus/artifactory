85d034691d22d24113b33874747637edbf5cf6e2
Ron Compos <rcompos@gmail.com>
2016-05-11 12:12:58 -0600

    Make file primary

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,46 +12,48 @@ class artifactory::config {
     require => Package[$::artifactory::package_name],
   }
 
+  ## TODO: Put back notify #notify  => Service['artifactory'],
   file { "${::artifactory::plugins_dir}/zip_upload.groovy":
     source  => 'puppet:///modules/artifactory/zip_upload.groovy',
     require => File[$::artifactory::plugins_dir],
-    #notify  => Service['artifactory'],
   }
 
   # Make sure jdbc driver  
   file { "$::artifactory::jdbc_file":
-    ensure => file,
-    owner  => artifactory,
-    group  => artifactory,
+    ensure  => file,
+    owner   => artifactory,
+    group   => artifactory,
+    source  => 'puppet:///modules/artifactory/ojdbc7.jar',
     require => Package[$::artifactory::package_name],
-  }  
-
-  # Make sure hanode 
-  file { "$::artifactory::hanode_file":
-    ensure => file,
-    source  => 'puppet:///modules/artifactory/ha-node.properties',
-    owner  => "artifactory",
-    group  => "artifactory",
-    mode   => '0664',
-    require => Package[$::artifactory::package_name],
-    #    notify  => Exec['edit_hanode0'],
   }
 
-#  exec { "edit_hanode0":
-#    command => "perl -pe 's/_node_id_/${::facts["hostname"]}/' $::artifactory::hanode_file",
-#    #onlyif  => 
-#    notify  => Exec['edit_hanode1'],
-#  }
-#
-#  exec { "edit_hanode1":
-#    command => "perl -pe 's/_cluster_home_/${::artifactory::cluster_home}/' ${::artifactory::hanode_file}",
-#    #onlyif  => 
-#    notify  => Exec['edit_hanode2'],
-#  }
-#
-#  exec { "edit_hanode2":
-#    command => "perl -pe 's/_ip_address_/${::facts["ip_address"]}/' ${::artifactory::hanode_file}",
-#    #onlyif  => 
-#  }
+  if ( $::artifactory::is_ha )  {
+    # Make sure hanode 
+    file { "$::artifactory::hanode_file":
+      ensure => file,
+      source  => 'puppet:///modules/artifactory/ha-node.properties',
+      owner  => "artifactory",
+      group  => "artifactory",
+      mode   => '0664',
+      require => Package[$::artifactory::package_name],
+    }
+  }
 
-}
+  #  exec { "edit_hanode0":
+  #    command => "perl -pe 's/_node_id_/${::facts["hostname"]}/' $::artifactory::hanode_file",
+  #    #onlyif  => 
+  #    notify  => Exec['edit_hanode1'],
+  #  }
+  #
+  #  exec { "edit_hanode1":
+  #    command => "perl -pe 's/_cluster_home_/${::artifactory::cluster_home}/' ${::artifactory::hanode_file}",
+  #    #onlyif  => 
+  #    notify  => Exec['edit_hanode2'],
+  #  }
+  #
+  #  exec { "edit_hanode2":
+  #    command => "perl -pe 's/_ip_address_/${::facts["ip_address"]}/' ${::artifactory::hanode_file}",
+  #    #onlyif  => 
+  #  }
+
+  }
