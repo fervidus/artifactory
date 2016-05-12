bddb1bf0fd4f6ea9317e810efddd66a705b338aa
Ron Compos <rcompos@gmail.com>
2016-05-12 10:10:51 -0600

    Added license debug message.

--- manifests/license.pp
+++ manifests/license.pp
@@ -3,20 +3,17 @@ class artifactory::license {
   # Only run if Pro
   # or Hi Availablity
   if $::artifactory::is_pro or $::artifactory::is_ha {
+
+    # for debug output on the puppet client
+    #notify {"Running with \$mysql_server_id ${mysql_server_id} ID defined":}
+    # for debug output on the puppet client - with full source information
+    notify{"license: ${::artifactory::license_key}":}
+
     artifactory_license { "http://${::facts['ipaddress']}:8081/artifactory":
       ensure   => present,
       license  => $::artifactory::license_key,
       user     => 'admin',
       password => 'password',
-
-      # for debug output on the puppet client
-      #notify {"Running with \$mysql_server_id ${mysql_server_id} ID defined":}
-
-      # for debug output on the puppet client - with full source information
-      notify {"license: \$license${mysql_server_id}":
-        withpath => true,
-      }
-
 
     }
   }
