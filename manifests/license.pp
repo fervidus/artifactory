f94ad8dd00bfaffc8f0060f3970c5ff84d93e1f8
Ron Compos <rcompos@gmail.com>
2016-05-12 09:54:29 -0600

    Added license debug message.

--- manifests/license.pp
+++ manifests/license.pp
@@ -8,6 +8,16 @@ class artifactory::license {
       license  => $::artifactory::license_key,
       user     => 'admin',
       password => 'password',
+
+      # for debug output on the puppet client
+      #notify {"Running with \$mysql_server_id ${mysql_server_id} ID defined":}
+
+      # for debug output on the puppet client - with full source information
+      notify {"license: \$license${mysql_server_id}":
+        withpath => true,
+      }
+
+
     }
   }
 }
