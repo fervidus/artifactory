3559720eb664866f55691dfdc9ed1ef19d22ea23
Bryan Belanger <bbelanger@azcender.com>
2016-06-22 14:13:36 -0400

    PLain Artifactory

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,32 +3,7 @@
 # This class is called from artifactory for install.
 #
 class artifactory::install {
-  # The YUM repos change based on whether or not installing pro
-  if ( $::artifactory::is_pro or $::artifactory::is_ha )  {
-    $yum_name = 'bintray-jfrog-artifactory-pro-rpms'
-    $yum_baseurl = 'https://jfrog.bintray.com/artifactory-pro-rpms'
-  }
-  else {
-    $yum_name = 'bintray-jfrog-artifactory-rpms'
-    $yum_baseurl = 'https://jfrog.bintray.com/artifactory-rpms'
-  }
-
-  # Add the jfrog yum repo
-  yumrepo { $yum_name:
-    baseurl  => $yum_baseurl,
-    descr    => $yum_name,
-    gpgcheck => 0,
-    enabled  => 1,
-  }
-
   package { $::artifactory::package_name:
     ensure  => present,
-    require => Yumrepo[$yum_name],
   }
-
-  #file { "${::artifactory::plugins_dir}/zip_upload.groovy":
-  #  source  => 'puppet:///modules/artifactory/zip_upload.groovy',
-  #  require => Package[$::artifactory::package_name],
-  #  notify  => Service['artifactory'],
-  #}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,26 +5,6 @@
 #
 class artifactory::params {
   
-  $is_pro               = false
-  $is_ha                = false
-  $license_key          = undef
-  $plugins_dir          = '/etc/opt/jfrog/artifactory/plugins'
-  $clusterhome          = undef
-  $artifactory_nic      = undef
-  $is_primary           = false
-  $membership_port      = 10001
-  $cluster_props        = 'cluster.properties'
-  $cluster_token        = 'detroit'
-  $arti_home           = '/var/opt/jfrog/artifactory'
-  #$jdbc_dir            = '$arti_home/tomcat/lib'
-  #$jdbc_file           = '$jdbc_dir/ojdbc7.jar'
-  $hanode_file         = "${arti_home}/etc/ha-node.properties"
-  #$jdbc_dir            = '/var/opt/jfrog/artifactory/tomcat/lib'
-  $jdbc_file            = '/var/opt/jfrog/artifactory/tomcat/lib/ojdbc7.jar'
-  $db_url               = undef
-  $db_user              = undef
-  $db_passwd            = undef
-
   case $::osfamily {
     'Debian': {
       $package_name = 'artifactory'
