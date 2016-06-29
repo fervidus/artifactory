5881f8c291688a0897ec87b3e9e8140795a1d64e
Bryan Belanger <bbelanger@azcender.com>
2016-06-28 20:42:37 -0400

    Artifactory mysql changes

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,4 +4,32 @@
 #
 class artifactory::config {
 
+
+  # Install storage.properties if Available
+  if(
+    $::artifactory::db_url or
+    $::artifactory::db_username or
+    $::artifactory::db_password or
+    $::artifactory::db_type
+    ) {
+    file { '/var/opt/jfrog/artifactory/etc/storage.properties':
+      ensure  => file,
+      content => epp(
+        'artifactory/oracle.properties.epp',
+        {
+          db_url                         => $::artifactory::db_url,
+          db_username                    => $::artifactory::db_username,
+          db_password                    => $::artifactory::db_password,
+          db_type                        => $::artifactory::db_type,
+          binary_provider_type           => $::artifactory::binaryvider_type,
+          pool_max_active                => $::artifactory::pool_max_active,
+          pool_max_idle                  => $::artifactory::pool_max_idle,
+          binary_provider_cache_maxSize  => $::artifactory::binaryvider_cache_maxSize,
+          binary_provider_filesystem_dir => $::artifactory::binaryvider_filesystem_dir,
+          binary_provider_cache_dir      => $::artifactory::binaryvider_cache_dir,
+        }
+      ),
+      mode    => '0664',
+    }
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -12,8 +12,18 @@
 #
 
 class artifactory(
-  String           $package_name     = $::artifactory::params::package_name,
-  String           $service_name     = $::artifactory::params::service_name,
+  String $package_name                                                    = $::artifactory::params::package_name,
+  String $service_name                                                    = $::artifactory::params::service_name,
+  Optional[Enum['mssql', 'mysql', 'oracle', 'postgresql']] $db_type       = $::artifactory::params::db_type,
+  Optional[String] $db_url                                                = $::artifactory::params::db_url,
+  Optional[String] $db_username                                           = $::artifactory::params::db_username,
+  Optional[String] $db_password                                           = $::artifactory::params::db_password,
+  Optional[Enum['filesystem', 'fullDb','cachedFS']] $binary_provider_type = $::artifactory::params::binary_provider_type,
+  Optional[Integer] $pool_max_active                                      = $::artifactory::params::pool_max_active,
+  Optional[Integer] $pool_max_idle                                        = $::artifactory::params::pool_max_idle,
+  Optional[Integer] $binary_provider_cache_maxSize                        = $::artifactory::params::binary_provider_cache_maxSize,
+  Optional[String] $binary_provider_filesystem_dir                        = $::artifactory::params::binary_provider_filesystem_dir,
+  Optional[String] $binary_provider_cache_dir                             = $::artifactory::params::binary_provider_cache_dir,
 ) inherits ::artifactory::params {
   contain ::artifactory::yum
   contain ::artifactory::install
