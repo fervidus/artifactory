5b9564abe66b8627cd04a08590f703de35c600ee
Bryan Belanger <bbelanger@azcender.com>
2016-06-29 22:51:54 -0400

    Fix db urls

--- manifests/config.pp
+++ manifests/config.pp
@@ -33,5 +33,10 @@ class artifactory::config {
       ),
       mode    => '0664',
     }
+
+    file { "${::artifactory_home}/lib/mysql-connector-java-5.1.39-bin.jar":
+      ensure => file,
+      source => 'puppet:///modules/artifactory/mysql-connector-java-5.1.39-bin.jar',
+    }
   }
 }
