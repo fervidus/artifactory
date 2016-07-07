1cfe53b0d614822129c7fb117c7212d95708972b
Bryan Belanger <bbelanger@azcender.com>
2016-07-07 13:07:32 -0400

    Cleaning up Artifactory basic.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,21 +12,24 @@
 #
 
 class artifactory(
-  String $package_name                                                    = $::artifactory::params::package_name,
-  String $service_name                                                    = $::artifactory::params::service_name,
-  Optional[Enum['mssql', 'mysql', 'oracle', 'postgresql']] $db_type       = $::artifactory::params::db_type,
-  Optional[Integer] $db_port                                              = $::artifactory::params::db_port,
-  Optional[String] $db_hostname                                           = $::artifactory::params::db_hostname,
-  Optional[String] $db_username                                           = $::artifactory::params::db_username,
-  Optional[String] $db_password                                           = $::artifactory::params::db_password,
-  Optional[Enum['filesystem', 'fullDb','cachedFS']] $binary_provider_type = $::artifactory::params::binary_provider_type,
-  Optional[Integer] $pool_max_active                                      = $::artifactory::params::pool_max_active,
-  Optional[Integer] $pool_max_idle                                        = $::artifactory::params::pool_max_idle,
-  Optional[Integer] $binary_provider_cache_maxSize                        = $::artifactory::params::binary_provider_cache_maxSize,
-  Optional[String] $binary_provider_filesystem_dir                        = $::artifactory::params::binary_provider_filesystem_dir,
-  Optional[String] $binary_provider_cache_dir                             = $::artifactory::params::binary_provider_cache_dir,
-) inherits ::artifactory::params {
+  String $yum_name                                                        = 'bintray-jfrog-artifactory-rpms',
+  String $yum_baseurl                                                     = 'https://jfrog.bintray.com/artifactory-rpms',
+  Optional[Enum['mssql', 'mysql', 'oracle', 'postgresql']] $db_type       = undef,
+  Optional[Integer] $db_port                                              = undef,
+  Optional[String] $db_hostname                                           = undef,
+  Optional[String] $db_username                                           = undef,
+  Optional[String] $db_password                                           = undef,
+  Optional[Enum['filesystem', 'fullDb','cachedFS']] $binary_provider_type = undef,
+  Optional[Integer] $pool_max_active                                      = undef,
+  Optional[Integer] $pool_max_idle                                        = undef,
+  Optional[Integer] $binary_provider_cache_maxSize                        = undef,
+  Optional[String] $binary_provider_filesystem_dir                        = undef,
+  Optional[String] $binary_provider_cache_dir                             = undef,
+) {
   $artifactory_home = '/var/opt/jfrog/artifactory'
+
+  $package_name     = 'artifactory'
+  $service_name     = 'artifactory'
 
   contain ::artifactory::yum
   contain ::artifactory::install
--- manifests/yum.pp
+++ manifests/yum.pp
@@ -3,13 +3,10 @@
 # This class is called from artifactory for install.
 #
 class artifactory::yum {
-  $yum_name = 'bintray-jfrog-artifactory-rpms'
-  $yum_baseurl = 'https://jfrog.bintray.com/artifactory-rpms'
-
   # Add the jfrog yum repo
-  yumrepo { $yum_name:
-    baseurl  => $yum_baseurl,
-    descr    => $yum_name,
+  yumrepo { $::artifactory::yum_name:
+    baseurl  => $::artifacotry::yum_baseurl,
+    descr    => $::artifactory::yum_name,
     gpgcheck => 0,
     enabled  => 1,
   }
