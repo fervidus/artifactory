98ee05d594e5101d04f796014ea444be922c8ceb
Bryan Belanger <bbelanger@azcender.com>
2016-07-08 12:35:11 -0400

    Fix storage condition checking

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,25 +13,37 @@ class artifactory::config {
     $::artifactory::db_password or
     $::artifactory::db_type
     ) {
-    file { "${::artifactory::artifactory_home}/etc/storage.properties":
-      ensure  => file,
-      content => epp(
-        'artifactory/storage.properties.epp',
-        {
-          db_port                        => $::artifactory::db_port,
-          db_hostname                    => $::artifactory::db_hostname,
-          db_username                    => $::artifactory::db_username,
-          db_password                    => $::artifactory::db_password,
-          db_type                        => $::artifactory::db_type,
-          binary_provider_type           => $::artifactory::binaryvider_type,
-          pool_max_active                => $::artifactory::pool_max_active,
-          pool_max_idle                  => $::artifactory::pool_max_idle,
-          binary_provider_cache_maxSize  => $::artifactory::binary_provider_cache_maxSize,
-          binary_provider_filesystem_dir => $::artifactory::binary_provider_filesystem_dir,
-          binary_provider_cache_dir      => $::artifactory::binary_provider_cache_dir,
+      if (
+        $::artifactory::db_hostname and
+        $::artifactory::db_port     and
+        $::artifactory::db_username and
+        $::artifactory::db_password and
+        $::artifactory::db_type
+        ) {
+        file { "${::artifactory::artifactory_home}/etc/storage.properties":
+          ensure  => file,
+          content => epp(
+            'artifactory/storage.properties.epp',
+            {
+              db_port                        => $::artifactory::db_port,
+              db_hostname                    => $::artifactory::db_hostname,
+              db_username                    => $::artifactory::db_username,
+              db_password                    => $::artifactory::db_password,
+              db_type                        => $::artifactory::db_type,
+              binary_provider_type           => $::artifactory::binaryvider_type,
+              pool_max_active                => $::artifactory::pool_max_active,
+              pool_max_idle                  => $::artifactory::pool_max_idle,
+              binary_provider_cache_maxSize  => $::artifactory::binary_provider_cache_maxSize,
+              binary_provider_filesystem_dir => $::artifactory::binary_provider_filesystem_dir,
+              binary_provider_cache_dir      => $::artifactory::binary_provider_cache_dir,
+            }
+          ),
+          mode    => '0664',
         }
-      ),
-      mode    => '0664',
+      }
+      else {
+        warn("Database port, hostname, username, password and type must eithier all be set, or non-set. Install will proceed without configuring storage.")
+      }
     }
 
     file { "${::artifactory::artifactory_home}/tomcat/lib/mysql-connector-java-5.1.39-bin.jar":
