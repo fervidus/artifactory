df07578dd1b2f3cc73b3d3a854eb037a5f508abd
Frank Wall <frank.wall@markt.de>
2017-10-19 13:46:18 +0200

    add $manage_repo parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,7 @@
 #
 class artifactory(
   Boolean $manage_java                                                    = true,
+  Boolean $manage_repo                                                    = true,
   String $yum_name                                                        = 'bintray-jfrog-artifactory-rpms',
   String $yum_baseurl                                                     = 'http://jfrog.bintray.com/artifactory-rpms',
   String $package_name                                                    = 'jfrog-artifactory-oss',
