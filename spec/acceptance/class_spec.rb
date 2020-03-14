bdb544d4f52604d80d103ced6c17c2ab6c5ece7c
Tim Meusel <tim@bastelfreak.de>
2020-03-14 13:34:46 +0100

    Enable Beaker tests on travis
    This ensures acceptance tests are executed within travis

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -21,5 +21,9 @@ describe 'artifactory class' do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
+
+    describe port(8081) do
+      it { is_expected.to be_listening }
+    end
   end
 end
